17edd20baf014f93bb444fe9dc88fdd20cfd2bad
metabot <metabot@ai.com>
2022-07-26 08:37:45 -0400

    first

---
+++ ansible/roles/os install/files/link.sh
@@ -0,0 +1,4 @@
+mkdir -p iso torr
+ln -srf Windows\ LTSC/19044.1288.211006-0501.21h2_release_svc_refresh_CLIENT_LTSC_EVAL_x64FRE_en-us.iso iso/
+ln -srf Fedora-i3-Live-x86_64-36/Fedora-i3-Live-x86_64-36-1.5.iso iso/
+ln -srf Fedora-Workstation-Live-x86_64-36/Fedora-Workstation-Live-x86_64-36-1.5.iso iso/
